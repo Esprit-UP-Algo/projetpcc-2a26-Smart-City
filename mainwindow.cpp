17f69128dc7d6e408ac604abf4d6a30bbd118d0b
nesrine-torkhani <nesrine.torkhani@esprit.tn>
2025-10-02 22:31:51 +0100

    weeek 3

---
+++ mainwindow.cpp
@@ -0,0 +1,33 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->label->setAttribute(Qt::WA_TranslucentBackground);
+    ui->label->setStyleSheet("background: transparent;");
+    ui->label->setAutoFillBackground(false);
+
+    // 👉 Bouton "Ajouter transaction" → page 0
+    connect(ui->btnAjouterVehicule, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentIndex(1);
+    });
+
+    // 👉 Bouton "Liste" → page 1
+    connect(ui->btnStatistique, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentIndex(2);
+    });
+
+    // 👉 Bouton "Statistique" → page 2
+    connect(ui->btnListe, &QPushButton::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentIndex(0);
+        // ou : ui->stackedWidget->setCurrentWidget(ui->pageStatistique);
+    });
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
