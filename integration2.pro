5013dfa1661654289f7551c04e9c24f86db96f5d
NourSaada <Nour.saada@esprit.tn>
2025-10-17 12:07:27 +0100

    Update menu_complet with latest changes

--- integration2.pro
+++ integration2.pro
@@ -9,6 +9,7 @@ CONFIG += c++17
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    connection.cpp \
     dashboardwindow.cpp \
     databasemanager.cpp \
     financespage.cpp \
@@ -26,6 +27,7 @@ SOURCES += \
     welcomedialog.cpp
 
 HEADERS += \
+    connection.h \
     dashboardwindow.h \
     databasemanager.h \
     financespage.h \
--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
-
+#include "connection.h"      // ➕ Ajout de l’en-tête de connexion
 #include <QApplication>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+
+    // ➕ Création et test de la connexion à la base
+    Connection c;
+    bool test = c.createconnect();
+
+    if (test) {
+        QMessageBox::information(nullptr, "Connexion", "Connexion réussie à la base de données !");
+        MainWindow w;
+        w.show();
+        return a.exec();
+    } else {
+        QMessageBox::critical(nullptr, "Erreur", "Connexion à la base de données échouée !");
+        return -1; // ❌ Stoppe le programme si la connexion échoue
+    }
 }
